5cc8a76db8f4785bcb47d515347aad9aebf651f2
hieunguyen7337 <59214551+hieunguyen7337@users.noreply.github.com>
2025-08-10 10:29:03 +1000

    remove unecessary stuff from main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,7 @@
-import os, time, shutil, random, threading, sys, msvcrt
-from colorama import init, Fore, Back, Style
-
-from Utils import clear_console, type_line, speak, center_text, load_ascii_faces
+from Utils import load_ascii_faces
 from Sequences import boot_sequence, load_AI, home
 
 face_frames = load_ascii_faces("images/ascii")
-
-# ANSI escape codes
-SAVE_CURSOR = '\033[s'
-RESTORE_CURSOR = '\033[u'
-CURSOR_UP = '\033[A'
-CLEAR_LINE = '\033[K'
-
 
 boot_sequence()
 load_AI(face_frames)
